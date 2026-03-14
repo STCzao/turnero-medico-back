d57dc9f90c8669fe63687bacd051a0de21b932be
STCzao <Santicoronel0183@gmail.com>
2026-03-13 23:51:30 -0300

    Basta, por favor

--- Program.cs
+++ Program.cs
@@ -40,7 +40,8 @@ if (!string.IsNullOrWhiteSpace(databaseUrl))
     // Npgsql necesita formato ADO.NET (Host=...;Database=...;Username=...;Password=...)
     var uri = new Uri(databaseUrl);
     var userInfo = uri.UserInfo.Split(':');
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    connectionString = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 }
 else
 {
